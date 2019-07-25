d5965dedd79dea10bb7781e90ef094a07d73d2c8
ignatioushunts <Pathor@88$Bagan>
2019-07-25 15:51:21 -0500

    Updated the Browser Helper With Description Added

--- src/test/groovy/com/automation/support/BrowserHelper.groovy
+++ src/test/groovy/com/automation/support/BrowserHelper.groovy
@@ -5,16 +5,32 @@ import org.openqa.selenium.WebDriver
 
 class BrowserHelper {
 
+    /**
+     * This is to Use the WebDriver from the BrowserHelper. So Driver doesn't needs to be call every time.
+     */
     static WebDriver driver = WebDriverHelper.getDriver()
 
+    //Navigation and Data Input Functionality Library
+
+    /**
+     * This Method Types in the Data in the Field.
+     */
     static def sendKeys(By by, String keys){
         driver.findElement(by).sendKeys(keys)
     }
 
+    /**
+     * This Method Clicks the Elements/Buttons of the UI.
+     */
     static def click(By by){
         driver.findElement(by).click()
     }
 
+    //Testing Functionality
+
+    /**
+     * This Method Verifies that if the Element Displayed in the UI.
+     */
     static boolean isElementDisplayed(By by) {
         try {
             driver.findElement(by)
