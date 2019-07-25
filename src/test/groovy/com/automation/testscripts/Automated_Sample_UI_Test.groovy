d941371b2be2ba880072fdf09d50136a957aa994
ignatioushunts <Pathor@88$Bagan>
2019-07-25 14:08:37 -0500

    Test Updated for Page Tittle Testing

--- src/test/groovy/com/automation/testscripts/Automated_Sample_UI_Test.groovy
+++ src/test/groovy/com/automation/testscripts/Automated_Sample_UI_Test.groovy
@@ -1,4 +1,35 @@
 package com.automation.testscripts
+import spock.lang.Specification
 
-class Automated_Sample_UI_Test {
+import static com.automation.support.WebDriverHelper.*
+
+
+class Automated_Sample_UI_Test extends Specification {
+
+    def setupSpec() {
+
+        //This is to Open the Target Website
+        openBrowser('https://www.google.com')
+    }
+
+    def cleanupSpec() {
+
+        //This is to Close the Web Browser When Test is completed
+        closeBrowser()
+    }
+
+    def "Automated Sample UI Test"() {
+
+        given:
+
+        String expectedTittle = "Google"
+
+        when: "Go to Google and Search"
+
+        String tittle = driver.getTitle()
+
+        then:
+
+        tittle == expectedTittle
+    }
 }
